786b1f219597d83d23421d399e6b5cbd4a9d8056
blairtchalla821@gmail.com <blairtchalla821@gmail.com>
2024-06-06 14:00:02 +0300

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    for n in num_list:
+      if n % 2 == 0:
+          print (n)
+      else :
+          print('null')
 
 def make_exclamation(sentence_list):
-    pass
+    for words in sentence_list:
+        print (f"{words}!")
+    
